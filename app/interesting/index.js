835e9a02148a4591daed1035c8370be372fe692f
Adam Sanderson <lazydayed@gmail.com>
2016-04-01 16:15:03 +0000

    initial dynamodb setup

--- app/interesting/index.js
+++ app/interesting/index.js
@@ -1,12 +1,17 @@
 'use strict';
 
-const pmongo = require('promised-mongo').compatible();
-const parse = require('co-body');
+var AWS = require("aws-sdk");
 
-const url = 'mongodb://0.0.0.0:27017/api_adamsandersoncouk';
+AWS.config.loadFromPath('./config.json');
+
+const parse = require('co-body');
+const uuid = require('uuid');
+
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 function shapeData (post) {
     return {
+        guid: uuid.v4(),
         created: new Date().getTime(),
         post: post
     }
@@ -17,36 +22,44 @@ function * create (req) {
 
     posts = posts.map(shapeData);
 
-    let db = pmongo(url, ['posts']);
-
-    yield db.posts.save(posts).then(db.close);
- 
-    db.close();
+    
  
     return Promise.resolve();
 }
 
+function readFromDatabase () {
+    return new Promise((resolve) => {
+        var params = {
+            TableName : "dev_api.adamsanderson.co.uk"
+        };
+        
+        docClient.scan(params, function(err, data) {
+            if (err) {
+                console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("Query succeeded.");
+                data.Items.forEach(function(item) {
+                    console.log(" -", item);
+                });
+            }
+            resolve();
+        });
+    });
+}
+
 function * read () {
-    let db = pmongo(url, ['posts']);
-    let posts = yield db.posts.find().toArray();
-    db.close();
-    return Promise.resolve(posts);
+    yield readFromDatabase();
+    return Promise.resolve();
 }
 
 function * allGroupByMonth () {
-    let db = pmongo(url, ['posts']);
-    let posts = yield db.posts.aggregate([
-        
-        ]);
-    db.close();
-    return Promise.resolve(posts);
+    
+    return Promise.resolve();
 }
 
 function * mostRecentPosts () {
-    let db = pmongo(url, ['posts']);
-    let posts = yield db.posts.find().limit(2).toArray();
-    db.close();
-    return Promise.resolve(posts);
+    
+    return Promise.resolve();
 }
 
 module.exports = {
---
+++ app/test/articleBuilder.test.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const tap = require("tap");

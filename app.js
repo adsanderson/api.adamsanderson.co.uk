035a179b8e9b7fa13c3e5329a553b53d6ce436ae
Adam Sanderson <lazydayed@gmail.com>
2016-04-06 14:42:25 +0000

    use env

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const fleekRouter = require('fleek-router');
 const app = koa();
 const argv = require('minimist')(process.argv.slice(2));
 
-const ip = argv.i || '0.0.0.0';
-const port = argv.p || '8080';
+// const ip = argv.i || '0.0.0.0';
+// const port = process.env.PORT || argv.p || '8080';
 
 fleekRouter(app);
 
-app.listen(port, ip);
-console.log(`listening on port ${port}`);
+app.listen(process.env.PORT);
+console.log(`listening on port ${process.env.PORT}`);

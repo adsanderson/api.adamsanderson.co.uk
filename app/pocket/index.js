2aacaf0840bd28118c8a3808397e65a56f002d8e
Adam Sanderson <lazydayed@gmail.com>
2016-04-06 10:43:49 +0000

    move to ENV variables

--- app/pocket/index.js
+++ app/pocket/index.js
@@ -10,13 +10,14 @@ const config = {
     'Content-Type': 'application/json'
   },
   body: JSON.stringify({
-    consumer_key: appConfig.pocket.CONSUMER_KEY, // eslint-disable-line camelcase
-    access_token: appConfig.pocket.ACCESS_TOKEN, // eslint-disable-line camelcase
+    consumer_key: process.env.CONSUMER_KEY, // eslint-disable-line camelcase
+    access_token: process.env.ACCESS_TOKEN, // eslint-disable-line camelcase
     tag: "reading"
   })
 };
 
 function fetchReadingList() {
+  console.log(config);
   return fetch('https://getpocket.com/v3/get', config)
     .then(response);
 }

4020e7f2b43a19798005eeb9d0c400a4605f2245
Adam Sanderson <lazydayed@gmail.com>
2016-04-07 11:34:34 +0000

    update routes and swagger docs

--- api_server.js
+++ api_server.js
@@ -4,12 +4,13 @@ const koa = require('koa');
 const fleekRouter = require('fleek-router');
 
 const app = koa();
-const argv = require('minimist')(process.argv.slice(2));
 
-// const ip = argv.i || '0.0.0.0';
-// const port = process.env.PORT || argv.p || '8080';
+fleekRouter(app, {
+    documentation: {
+      swagger: `${process.cwd()}/api.json`,
+      route: '/'
+    }
+});
 
-fleekRouter(app);
-
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, process.env.IP);
 console.log(`listening on port ${process.env.PORT}`);
---
+++ app/test/swagger/swagger.test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const test = require('tap').test;
+var spec = require('swagger-tools').specs.v2;
+const api = require('../../../api.json');
+
+test('validate swagger', t => {
+  spec.validate(api, function(err, result) {
+    if (err) {
+      throw err;
+    }
+
+    if (typeof result !== 'undefined') {
+      if (result.errors.length > 0) {
+        console.log('The Swagger document is invalid...');
+
+        console.log('');
+
+        console.log('Errors');
+        console.log('------');
+
+        result.errors.forEach(function(err) {
+          console.log('#/' + err.path.join('/') + ': ' + err.message);
+        });
+
+        console.log('');
+      }
+
+      if (result.warnings.length > 0) {
+        console.log('Warnings');
+        console.log('--------');
+
+        result.warnings.forEach(function(warn) {
+          console.log('#/' + warn.path.join('/') + ': ' + warn.message);
+        });
+      }
+      // if (result.errors.length > 0) {
+      //   process.exit(1);
+      // }
+    }
+
+    t.notOk(result, 'swagger validator returned error/warning results');
+    t.end();
+  });
+});

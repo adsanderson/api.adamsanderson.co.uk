3877369e1b5f0485f97c55b79be6af4ff6fa075e
Adam Sanderson <lazydayed@gmail.com>
2016-05-03 12:44:24 +0000

    add excerpt to generated monthly article

--- test/interesting/articleBuilder.test.js
+++ test/interesting/articleBuilder.test.js
@@ -11,14 +11,21 @@ function contains(article, content) {
 test('articleBuilder: ', t => {
   let articles = articleBuilder(createArticles());
   const numberOfArticles = Object.keys(articles).length;
-  const singleArticle = articles['2016-04'];
+  const singleArticleWithExcerpt = articles['2016-04'];
+  const singleArticle = articles['2016-03'];
+
+  console.log(singleArticleWithExcerpt);
 
   const frontMatterHeader = contains(singleArticle, '---');
+  const doesNotContainExcerpt = contains(singleArticle, '<!-- more -->');
+  const containsExcerpt = contains(singleArticleWithExcerpt, '<!-- more -->');
 
   t.notEqual(frontMatterHeader, -1, 'expect to contain front matter');
 
   t.equal(numberOfArticles, 3, 'expect *2* groups and latest');
   t.type(singleArticle, 'string', 'expect the group to be a string');
+  t.equal(doesNotContainExcerpt, -1, 'expect to contain excerpt break');
+  t.notEqual(containsExcerpt, -1, 'expect to contain excerpt break');
   t.ok(articles.latest);
   t.type(articles.latest, 'string');
 

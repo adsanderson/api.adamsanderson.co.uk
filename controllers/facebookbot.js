3a0062ed05fa71f0bae66e06a86f413817f4a7ee
Adam Sanderson <lazydayed@gmail.com>
2016-05-12 15:53:48 +0000

    reorder responses

--- controllers/facebookbot.js
+++ controllers/facebookbot.js
@@ -2,10 +2,12 @@
 
 // const interesting = require("../app/facebookbot");
 
-module.exports.get = function * () {
+module.exports.get = function * (next) {
   console.log('///////////', this.request.query['hub.verify_token'], process.env.VERIFY_TOKEN);
+
+  this.body = 'Error, wrong validation token';
+
   if (this.request.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
     this.body = this.request.query['hub.challenge'];
   }
-  this.body = 'Error, wrong validation token';
 };

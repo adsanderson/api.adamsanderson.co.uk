a0ce4f60e4f9b8e7cf1713b4066f7e7cb79f87f3
Adam Sanderson <lazydayed@gmail.com>
2016-05-12 15:28:23 +0000

    add facebook verify

--- controllers/facebookbot.js
+++ controllers/facebookbot.js
@@ -3,7 +3,7 @@
 // const interesting = require("../app/facebookbot");
 
 module.exports.get = function * () {
-  if (this.request.query['hub.verify_token'] === '') {
+  if (this.request.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
     this.body = this.request.query['hub.challenge'];
   }
   this.body = 'Error, wrong validation token';

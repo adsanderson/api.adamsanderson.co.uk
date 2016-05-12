8ed6f2318462086f67898f934dc664743bcc3b85
Adam Sanderson <lazydayed@gmail.com>
2016-05-12 15:40:14 +0000

    add logging

--- controllers/facebookbot.js
+++ controllers/facebookbot.js
@@ -3,6 +3,7 @@
 // const interesting = require("../app/facebookbot");
 
 module.exports.get = function * () {
+  console.log('///////////', this.request.query['hub.verify_token'], process.env.VERIFY_TOKEN);
   if (this.request.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
     this.body = this.request.query['hub.challenge'];
   }

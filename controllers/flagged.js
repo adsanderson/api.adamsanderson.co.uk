d2295eaae3a132a775fa780dd5535d9ef11f12f7
Adam Sanderson <lazydayed@gmail.com>
2016-06-20 13:01:15 +0000

    clean debugging

--- controllers/flagged.js
+++ controllers/flagged.js
@@ -3,8 +3,5 @@
 const getFlag = require("../app/flagged");
 
 module.exports.get = function * () {
-  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!, TEST if this gets hit');
-  this.body = {
-    result: getFlag(this.request.query.flag)
-  };
+  this.body = getFlag(this.request.query.flag);
 };

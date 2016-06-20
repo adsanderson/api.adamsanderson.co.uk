0361f2189271d8604016b2118320b61c519aab70
Adam Sanderson <lazydayed@gmail.com>
2016-06-20 10:56:01 +0000

    add debug code

--- controllers/flagged.js
+++ controllers/flagged.js
@@ -3,6 +3,7 @@
 const getFlag = require("../app/flagged");
 
 module.exports.get = function * () {
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!, TEST if this gets hit');
   this.body = {
     result: getFlag(this.request.query.flag)
   };

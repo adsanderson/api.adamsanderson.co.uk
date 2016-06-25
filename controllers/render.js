bae453a3cc7e230099c4cd844e971513540facb3
Adam Sanderson <lazydayed@gmail.com>
2016-06-25 17:00:31 +0000

    add git hooks

--- controllers/render.js
+++ controllers/render.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const barcode = require("../app/barcode");
+// const render = require("../app/render");
 
 module.exports.get = function * () {
-  //let barcodes = yield barcode.read(this.request.query);
+  // let barcodes = yield barcode.read(this.request.query);
   this.body = {
     message: 'rendered DOM',
     DOMString: ''

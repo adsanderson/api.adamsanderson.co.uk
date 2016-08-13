a020a5aec0abbb14194da5dced23a6f91a7d03b6
Adam Sanderson <adam@adamsanderson.co.uk>
2016-08-13 15:16:10 +0100

    Tidy deps

---
+++ app/time-logger/index.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const test = require('ava').test;
+
+const timeLogger = require('./index');
+
+test('flagged:', t => {
+  t.equal(typeof timeLogger, 'function', 'is a function');
+  t.equal(typeof timeLogger(), 'string', 'expect a string to be returned');
+  t.end();
+});

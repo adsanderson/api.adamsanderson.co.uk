1cb6366877a9b8b0ee559ec3d97c083790ddae5e
Adam Sanderson <lazydayed@gmail.com>
2017-03-03 17:35:32 +0000

    tidy comments and debug

--- api_server.js
+++ api_server.js
@@ -5,28 +5,13 @@ const fleekCtx = require('fleek-context')
 const fleekRouter = require('fleek-router')
 const api = require('./swagger')
 
-console.log(fleekRouter)
-
 const app = new Koa()
 
 app.use(fleekCtx(api))
 
 let router = new fleekRouter.Router()
-// fleekRouter({ controllers: `${__dirname}/controllers` });
-
-// fleekRouter(app, {
-//   swagger: api,
-//   documentation: {
-//     swagger: api,
-//     route: '/'
-//   }
-// })
 
 app.use(router.controllers(`${__dirname}/controllers`))
-
-app.use(ctx => {
-  ctx.body = 'Hello World'
-})
 
 app.listen(process.env.PORT, process.env.IP)
 console.log(`listening on port ${process.env.IP}:${process.env.PORT}`)

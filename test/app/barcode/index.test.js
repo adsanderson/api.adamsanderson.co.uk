32739b77afa0f38e4a6dc06b10275ebef2666b9c
Adam Sanderson <lazydayed@gmail.com>
2017-03-04 11:05:40 +0000

    restructure async test

--- test/app/barcode/index.test.js
+++ test/app/barcode/index.test.js
@@ -20,12 +20,15 @@ test('barcode:', t => {
   t.is(typeof barcode.read, 'function', 'has read function')
 })
 
-test('barcode: read', async (t) => {
+test('barcode: read', (t) => {
   t.plan(2)
 
-  const result = await barcode.read(['test-test', 'foo-bar'])
+  const test = async () => {
+    const result = await barcode.read(['test-test', 'foo-bar'])
+    const singleFinalBarcodeKeys = Object.keys(result[0])
+    t.not(singleFinalBarcodeKeys.indexOf('text'), -1, 'expect `text` property to exist')
+    t.not(singleFinalBarcodeKeys.indexOf('barcode'), -1, 'expect `barcode` property to exist')
+  }
 
-  const singleFinalBarcodeKeys = Object.keys(result[0])
-  t.not(singleFinalBarcodeKeys.indexOf('text'), -1, 'expect `text` property to exist')
-  t.not(singleFinalBarcodeKeys.indexOf('barcode'), -1, 'expect `barcode` property to exist')
+  test()
 })

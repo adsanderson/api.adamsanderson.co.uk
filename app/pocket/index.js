a109751907c463f6e622c556a579a04e8d59d742
Adam Sanderson <lazydayed@gmail.com>
2017-03-12 18:51:52 +0000

    updated auth

--- app/pocket/index.js
+++ app/pocket/index.js
@@ -3,23 +3,30 @@
 const fetch = require('node-fetch')
 const response = require('./response')
 
-const config = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    consumer_key: process.env.CONSUMER_KEY, // eslint-disable-line camelcase
-    access_token: process.env.ACCESS_TOKEN, // eslint-disable-line camelcase
-    state: 'all',
-    tag: 'reading'
-  })
+function pocketGet () {
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      consumer_key: process.env.CONSUMER_KEY, // eslint-disable-line camelcase
+      access_token: process.env.ACCESS_TOKEN, // eslint-disable-line camelcase
+      state: 'all',
+      tag: 'reading'
+    })
+  }
+  return fetch('https://getpocket.com/v3/get', config)
 }
 
-function fetchReadingList () {
+async function fetchReadingList () {
   console.log('fetching pocket articles tagged "reading"')
-  return fetch('https://getpocket.com/v3/get', config)
-    .then(response)
+
+  const posts = await pocketGet()
+  const result = await response(posts)
+
+  return result
 }
 
 module.exports = fetchReadingList
